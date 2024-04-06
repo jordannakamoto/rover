6b5834802af16844cdc9cddc10b9a6fd1e52209c
jordannakamoto <jordannakamoto@gmail.com>
2024-04-06 14:46:39 -0700

    rover chagnes

--- Raspi/rover.py
+++ Raspi/rover.py
@@ -75,13 +75,23 @@ def toggle_conveyor_operation():
   global conveyor_process
 
   if conveyor_process:
-      print("Stopping conveyor... can take 4s")
+      print("Stopping conveyor...")
       conveyor_process.terminate()  # Terminate the process
       conveyor_process.wait()  # Wait for process to terminate
       conveyor_process = None
   else:
       print("Starting conveyor...")
       conveyor_process = subprocess.Popen(['python3', 'conveyor1.py'])
+
+# just kill a conveyor process if its running
+def kill_conveyor():
+  global conveyor_process
+
+  if conveyor_process:
+      print("Stopping conveyor...")
+      conveyor_process.terminate()  # Terminate the process
+      conveyor_process.wait()       # Wait for process to terminate
+      conveyor_process = None
 
 def flush_input():
     try:
@@ -100,6 +110,7 @@ def print_instructions():
   print("'o' - Toggle conveyor")
   print("'a' - Reset Bucket")
   print("'d' - Dump Bucket")
+  print("'0' - Quit")
   print("Commands will lock input during operation.\n")
 
 # Open a handle to "/dev/i2c-3", representing the I2C bus.
@@ -137,6 +148,7 @@ while True:
     elif (x == 'j'):
         beltStep.homeRev()
         beltPosition = 0
+        kill_conveyor()
         time.sleep(belt_move_duration * beltPosition)
     elif (x == 'a'):
         dumpBucket.homeRev()
@@ -146,6 +158,11 @@ while True:
         time.sleep(dump_homing_duration)
     elif (x == 'o'):
         toggle_conveyor_operation()
+    elif (x == '0'):
+        kill_conveyor()
+        time.sleep(2)
+        exit()
+
     flush_input()
     os.system('clear') # clear the screen
     print("Ready")
